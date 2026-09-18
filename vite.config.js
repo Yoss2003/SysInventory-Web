75c0b482be4c7291d34ed851904e92c0ddc46fe3
Alexandro-dev <mercadogarciaalexandro10@gmail.com>
2026-09-18 16:16:47 -0500

    Soporte para versiones antiguas IOS

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11', 'iOS >= 12']
+    })
+  ],
 })
